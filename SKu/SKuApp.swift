c067d8e82c10bb22faee52aed55176d10fa3e1cd
Sean Hughes <sneakerheadsean@gmail.com>
2022-08-08 10:51:18 -0400

    Initial Commit

---
+++ SKu/SKuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SKuApp.swift
+//  SKu
+//
+//  Created by Sean Hughes on 8/8/22.
+//
+
+import SwiftUI
+
+@main
+struct SKuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
